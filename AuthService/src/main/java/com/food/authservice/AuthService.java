ec28311f77e37c4bfded8f70d80116a5f0c629c3
Nikhils14 <nikhilsingh93p@gmail.com>
2025-10-12 00:40:59 +0530

    V1

--- AuthService/src/main/java/com/food/authservice/AuthService.java
+++ AuthService/src/main/java/com/food/authservice/AuthService.java
@@ -1,12 +1,17 @@
 package com.food.authservice;
 
 import com.food.authservice.Request.AuthRequest;
+import com.food.authservice.Request.SignupRequest;
 import com.food.authservice.entity.AppUser;
+import com.food.authservice.entity.RefreshToken;
+import com.food.authservice.entity.Role;
 import com.food.authservice.response.AuthResponse;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public AuthResponse login(AuthRequest request) {
         AppUser user;
@@ -32,13 +38,62 @@ public class AuthService {
     }
 
     public AuthResponse refreshToken(String refreshToken) {
-        Claims claims = jwtService.extractClaims(refreshToken);
-        String email = claims.getSubject();
 
-        AppUser user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        RefreshToken storedToken = refreshTokenRepository.findByToken(refreshToken)
+                .orElseThrow(() -> new RuntimeException("Invalid refresh token"));
 
+
+        if (storedToken.getExpiryDate().isBefore(Instant.now())){
+            refreshTokenRepository.delete(storedToken);
+            throw new RuntimeException("Refresh token expired");
+        }
+
+        AppUser user = storedToken.getUser();
         String newAccessToken = jwtService.generateAccessToken(user);
         return new AuthResponse(newAccessToken, refreshToken);
+
+//        Claims claims = jwtService.extractClaims(refreshToken);
+//        String email = claims.getSubject();
+//
+//        AppUser user = userRepository.findByEmail(email)
+//                .orElseThrow(() -> new RuntimeException("User not found"));
+//
+//        String newAccessToken = jwtService.generateAccessToken(user);
+//        return new AuthResponse(newAccessToken, refreshToken);
+    }
+
+    private String saveRefreshToken (AppUser user){
+        String token = jwtService.generateRefreshToken(user);
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .token(token)
+                .user(user)
+                .expiryDate(Instant.now().plusSeconds(7*24*60*60))
+                .build();
+
+        refreshTokenRepository.deleteByUser(user);
+        refreshTokenRepository.save(refreshToken);
+
+        return  token;
+    }
+
+    public AuthResponse signUp(SignupRequest request){
+        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new RuntimeException("Email already register");
+        }
+
+        AppUser user = AppUser.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole() != null ? request.getRole() : Role.CUSTOMER)
+                .build();
+
+        userRepository.save(user);
+
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return new AuthResponse(accessToken, refreshToken);
     }
 }
--- AuthService/src/main/java/com/food/authservice/Request/AuthRequest.java
+++ AuthService/src/main/java/com/food/authservice/Request/AuthRequest.java
@@ -1,9 +1,16 @@
 package com.food.authservice.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+
+    @Email(message = "Invalid email fromat")
+    @NotBlank(message =  "Email is required")
     private String email;
+
+    @NotBlank(message =  "Password is required")
     private String password;
 }
